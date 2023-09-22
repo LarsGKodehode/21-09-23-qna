54b7c2e7c82ae55cfb73768ccd771f6fc0e1e307
LarsG <lars.gunnar@jobloop.no>
2023-09-22 08:17:03 +0000

    add links

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom'
+import { blogPosts } from '../data/blogPosts'
+
 export function HomePage() {
   return (
     <>
       <h2>Home Page</h2>
+
+      <ul>
+        {
+          blogPosts.map(post => {
+            return (
+              <li key={post.id}>
+                <Link to={`/blog/${post.id}`}>
+                  {post.title}
+                </Link>
+              </li>
+            )
+          })
+        }
+      </ul>
+
     </>
   )
 }
